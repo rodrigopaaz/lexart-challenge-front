7893f03ce44000352ca113dc981a74d552630f54
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-07-15 23:56:53 -0300

    feat: create first chatbot messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/chat.css'
+import { chatBot } from '../utils/chatBot'
 import Message from './Message'
 
 export default function Chat () {
-  const messages = [
-    { id: 1, user: 'rodrigo', text: 'alo' },
-    { id: 2, user: 'bot', text: 'vamo que vamo' }
-  ]
+  const [text, setText] = useState('')
+  const [messages, setMessages] = useState([])
+
+  const handleMessage = () => {
+    const newMessage = {
+      id: messages.length + 1,
+      user: 'rodrigo',
+      text
+    }
+
+    const updatedMessages = [...messages, newMessage]
+    setMessages(updatedMessages)
+    setText('')
+
+    if (updatedMessages.length >= 1) {
+      const botResponse = {
+        id: updatedMessages.length + 1,
+        user: 'bot',
+        text: chatBot(updatedMessages)
+      }
+      setMessages([...updatedMessages, botResponse])
+    }
+  }
+
   return (
     <div className='chat-container'>
       <div className='chat-message-bot-avatar'>.</div>
@@ -17,7 +38,15 @@ export default function Chat () {
 
       </div>
       <div className='chat-message-input-container'>
-        <input type="text" />
+        <input type="text" value={text}
+          onChange={({ target: { value } }) => setText(value)}
+          onKeyDown={({ key }) => {
+            if (key === 'Enter') handleMessage()
+          }}
+        />
+        <button
+          onClick={() => handleMessage()}
+        >send</button>
       </div>
     </div>
   )
--- src/components/Message.js
+++ src/components/Message.js
@@ -4,12 +4,25 @@ import '../styles/message.css'
 export default function Message (data) {
   const { message } = data
   const classAlign = message.user === 'bot' ? 'left' : 'right'
+  const isMessageArray = Array.isArray(message.text)
+
   return (
     <div className={`message-container-${classAlign}`}>
-      <div>
-        {message.text}
+      <div className={isMessageArray ? 'message-container-vertical' : null}>
+        {isMessageArray
+          ? (
+            <ul>
+              {message.text.map(({ message, url }, index) => (
+                <li key={index}>
+                  <a href={url} target='_blank' rel="noreferrer">{message}</a>
+                </li>
+              ))}
+            </ul>
+          )
+          : (
+            message.text
+          )}
       </div>
     </div>
-
   )
 }
